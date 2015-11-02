e2340a2fe0fb3e123e2173411f813c20764f792d
elijj <elij@uw.edu>
2015-11-02 13:52:05 -0800

    mad lib

--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,5 +1,20 @@
 // Create application
+var myApp = angular.module('myApp', [])
 
+// Bind controller myCtrl
+myApp.controller('myCtrl', function($scope){
+    $scope.user = {
+        username: 'nameless :(',
+        age : 'over 9000',
+        userLocation : 'the moon',
+        food : 'nothing, we are on a diet..'
+    }
+    //var name = $scope.user.username 
+    //var email = $scope.user.email
+    $scope.submitForm = function() {
+        console.log('Name: ' + $scope.user.username + '   Email:' + $scope.user.age + '   Location:' + $scope.user.userLocation + '   Food:' + $scope.user.food )
+    }
+})
 
 // Bind controller madCtrl
 
